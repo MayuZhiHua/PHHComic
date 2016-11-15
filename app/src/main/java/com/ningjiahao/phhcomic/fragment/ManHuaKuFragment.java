ab969e3bb815ce877c478a61e5afdcd90387275d
ConanHolmes <736116737@qq.com>
2016-11-15 09:27:30 +0800

    第二次修改：宁家豪 ManHuaKufragment

---
+++ app/src/main/java/com/ningjiahao/phhcomic/fragment/ManHuaKuFragment.java
@@ -0,0 +1,30 @@
+package com.ningjiahao.phhcomic.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ningjiahao.phhcomic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ManHuaKuFragment extends Fragment {
+
+
+    public ManHuaKuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_man_hua_ku, container, false);
+    }
+
+}
