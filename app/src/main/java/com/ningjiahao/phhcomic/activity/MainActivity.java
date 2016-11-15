a7da0c702dd298de06717004090f729000c2928d
My <1060043814@qq.com>
2016-11-15 09:43:33 +0800

    test

--- app/src/main/java/com/ningjiahao/phhcomic/activity/MainActivity.java
+++ app/src/main/java/com/ningjiahao/phhcomic/activity/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import com.ningjiahao.phhcomic.R;
 
 public class MainActivity extends AppCompatActivity {
-//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
