c95ff13af18a05e90028ffeae09d2845bd1423dd
YuZhiHua <boygod@outlook.com>
2016-11-21 11:21:54 +0800

    瞬间移动怕不怕

--- app/src/main/java/com/ningjiahao/phhcomic/fragment/FindFragment.java
+++ app/src/main/java/com/ningjiahao/phhcomic/fragment/FindFragment.java
@@ -1,35 +1,12 @@
 package com.ningjiahao.phhcomic.fragment;
 
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.ningjiahao.phhcomic.R;
 import com.ningjiahao.phhcomic.activity.SearchActivity;
 import com.ningjiahao.phhcomic.adapter.FindPagerAdapter;
 import com.ningjiahao.phhcomic.base.BaseFragment;
-import com.ningjiahao.phhcomic.bean.FindSearchTitleBean;
-import com.ningjiahao.phhcomic.config.URLConstants;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
